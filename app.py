deab48365de80247e44d7c2025bd2bc674a88aae
shaocongma <ccbora742@gmail.com>
2023-06-08 06:47:02 +0800

    Add exception catch.

--- app.py
+++ app.py
@@ -2,20 +2,24 @@ import gradio as gr
 from evaluate_embeddings import generate_title_abs, embed
 from papers_ranking import get_top_k
 
-
 SLIDER_DEFAULT = 30
 SLIDER_MAXIMUM = 50
 
-def get_embeddings(paper_title, include_embeddings = False):
-    output = generate_title_abs(paper_title)
 
+def get_embeddings(paper_title, include_embeddings=False):
+    try:
+        output = generate_title_abs(paper_title)
+    except:
+        print("Failed to generate tilte. Use default value instead.")
+        output = [{"title": paper_title, "abstract": paper_title, "paper_id": "target_paper"}]
     emb_vector = embed(output)["target_paper"]
     target_paper = output[0]
     if include_embeddings:
         target_paper["embeddings"] = emb_vector
     return target_paper
 
-def get_relavant_papers(paper_title, papers_file, k, include_embeddings = False):
+
+def get_relavant_papers(paper_title, papers_file, k, include_embeddings=False):
     # output = generate_title_abs(paper_title)
     #
     # emb_vector = embed(output)["query_paper"]
@@ -30,14 +34,15 @@ def get_relavant_papers(paper_title, papers_file, k, include_embeddings = False)
     if not include_embeddings:
         for key in top_k_papers:
             top_k_papers[key].pop("embeddings", None)
-    return top_k_papers # get_top_k(papers_string, target_paper, k)
+    return top_k_papers  # get_top_k(papers_string, target_paper, k)
 
 
 def clear(text):
     return ""
+
+
 def accordion_clear(file, slider):
     return None, SLIDER_DEFAULT
-
 
 
 with gr.Blocks() as demo:
@@ -50,7 +55,6 @@ with gr.Blocks() as demo:
     """)
     include_embedding = gr.Checkbox(value=False, visible=False, label="Include Embedding?")
     with gr.Row():
-
         with gr.Column():
             paper_title = gr.Textbox(label="Title")
             with gr.Row():
@@ -64,15 +68,13 @@ with gr.Blocks() as demo:
                     btn_accordion_clear = gr.Button("Clear")
                     btn_relevant_papers = gr.Button("Get Top-k Papers", variant="primary")
 
-
         with gr.Column():
             out_json = gr.JSON()
 
-
     btn_clear.click(fn=clear, inputs=[paper_title],
-                             outputs=[paper_title])
+                    outputs=[paper_title])
     btn_accordion_clear.click(fn=accordion_clear, inputs=[papers_json, slider],
-                             outputs=[papers_json, slider])
+                              outputs=[papers_json, slider])
 
     btn_get_embeddings.click(fn=get_embeddings, inputs=[paper_title, include_embedding],
                              outputs=[out_json], api_name="get_embeddings")
